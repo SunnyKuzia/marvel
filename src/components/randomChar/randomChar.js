a9dad7682df1c8b56d515f8aee06dc2702e20e77
Alexandra Huliakevich <sunnykuzia1996@gmail.com>
2025-08-05 20:10:16 +0300

    feat: add Spinner and ErrorMessage components

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner'
+import ErrorMessage from '../errorMessage/ErrorMessage';
+
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png'
@@ -11,48 +14,50 @@ class RandomChar extends Component {
         this.updateChar();
     }
 
-    state = { // state можно прописывать как св-во (без конструктора)
-        char: {}
+    state = { // state можно прописывать как св-во (вне конструктора)
+        char: {},
+        loading: true,
+        error: false
     }
 
     marvelService = new MarvelService();
 
     onCharLoaded = (char) => {
-        this.setState({ char });
+        this.setState({
+            char,
+            loading: false
+        });
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        });
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * 20);
         this.marvelService
             .getCharacter(id)
-            .then(this.onCharLoaded);
-        // в then приходит аргумент из promise и в this.onCharLoaded автоматически он подставится
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+        // в then приходит аргумент из promise и в this.onCharLoaded автоматически он подставится (поэтому не прописываем char в аргументах)
     }
 
     render() {
 
-        const { char: { name, description, thumbnail, homepage, wiki } } = this.state;
+        const { char, loading, error } = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || errorMessage) ? <View char={char} /> : null;
 
         return (
-            <div className='randomchar'>
-                <div className='randomchar__block'>
-                    <img src={thumbnail} alt="Random character" className='randomchar__img' />
-                    <div className='randomchar__info'>
-                        <div className="randomchar__name">{name}</div>
-                        <div className="randomchar__descr">
-                            {description}
-                        </div>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className='button button__main'>
-                                <div className="inner">HOMEPAGE</div>
 
-                            </a>
-                            <a href={wiki} className='button button__secondary'>
-                                <div className="inner">WIKI</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+            <div className='randomchar'>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br />
@@ -68,6 +73,31 @@ class RandomChar extends Component {
             </div >
         );
     }
+}
+
+
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki } = char;
+    return (
+        <div className='randomchar__block'>
+            <img src={thumbnail} alt="Random character" className='randomchar__img' />
+            <div className='randomchar__info'>
+                <div className="randomchar__name">{name}</div>
+                <div className="randomchar__descr">
+                    {description}
+                </div>
+                <div className="randomchar__btns">
+                    <a href={homepage} className='button button__main'>
+                        <div className="inner">HOMEPAGE</div>
+
+                    </a>
+                    <a href={wiki} className='button button__secondary'>
+                        <div className="inner">WIKI</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default RandomChar;
